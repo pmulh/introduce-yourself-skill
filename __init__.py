090765ce7f5fced39b573007b82aba9a590e0a06
Peter-TaliPi <pmulholland15@outlook.com>
2021-07-26 12:09:31 +0100

    Initial commit

--- __init__.py
+++ __init__.py
@@ -8,6 +8,11 @@ class IntroduceYourself(MycroftSkill):
     @intent_file_handler('yourself.introduce.intent')
     def handle_yourself_introduce(self, message):
         self.speak_dialog('yourself.introduce')
+        # TODO - wait for response and use name to say 'hello <name>'
+        self.speak_dialog('pleasantries_1')
+        # TODO - add a special response when the name received above is 'David' or 'Dave'
+        # Something like 'Hi David, it's nice to me-- I'm sorry Dave, I'm afraid I can't do that'
+        self.speak_dialog('pleasantries_2')
 
 
 def create_skill():
