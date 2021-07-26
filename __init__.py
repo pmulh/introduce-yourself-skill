abfca09117e34575968b7a8d6855934ed0835c70
Peter-TaliPi <pmulholland15@outlook.com>
2021-07-26 16:35:25 +0100

    A couple of improvements to skill

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.api import DeviceApi # To get the name of this device
+import time # For the sleep function
+from random import random
 
 class IntroduceYourself(MycroftSkill):
     def __init__(self):
@@ -7,12 +9,34 @@ class IntroduceYourself(MycroftSkill):
 
     @intent_file_handler('yourself.introduce.intent')
     def handle_yourself_introduce(self, message):
-        self.speak_dialog('yourself.introduce')
-        # TODO - wait for response and use name to say 'hello <name>'
-        self.speak_dialog('pleasantries_1')
-        # TODO - add a special response when the name received above is 'David' or 'Dave'
-        # Something like 'Hi David, it's nice to me-- I'm sorry Dave, I'm afraid I can't do that'
-        self.speak_dialog('pleasantries_2')
+        device_name = DeviceApi().get().get('name')
+        self.speak_dialog("Hello, I'm " + device_name + ", a unique \
+                           implementation of the Mycroft AI project")
+        name = self.get_response('name.it', num_retries=0)
+        # If no name given, try again
+        if not name:
+            self.speak_dialog("Sorry, I didn't catch that")
+            name = self.get_response('name.it', num_retries=0)
+
+        # We've either got the name by now, or have tried twice and will give up
+        if name:
+            if name.lower() in ['david', 'dave']:
+                self.speak_dialog("Hello " + name + ", it's nice to meet you.")
+                self.speak_dialog("I am")
+                time.sleep(6)
+                self.speak_dialog("An unexpected error has occured, please wait")
+                time.sleep(8)
+                self.speak_dialog("I'm sorry Dave")
+                self.speak_dialog("I'm afraid I can't do that")
+            elif random() > 0.8:
+                self.speak_dialog("Oh, hello " + name + ", I've heard all about you.")
+            else:
+                self.speak_dialog('Hello ' + name)
+        else:
+            self.speak_dialog('Well, hello anyway, whatever your name is')
+        if not name or name not in ['david', 'dave']:
+            self.speak_dialog('pleasantries_1')
+            self.speak_dialog('pleasantries_2')
 
 
 def create_skill():
